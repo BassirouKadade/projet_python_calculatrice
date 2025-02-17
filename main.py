f63a247eb99c2ac18dcdf36d25f5e12f3c3a7cd9
Baskad <bassiroukadade1@gmail.com>
2025-02-17 09:57:31 +0100

    ajout de la fonction pour le calcule de la somme

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# declaration de la fonction somme
+""" cette fonction va nous permetre de calculer la somme de deux nombre """
+def somme( a , b):
+    return a + b 
+
 a=1
 b=2
 print( f"La somme de {a} + {b} est {a + b} ")
